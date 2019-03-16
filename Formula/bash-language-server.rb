d8f90f9db04392e7ca422635632716b27d37f073
Ben Wainwright <ben.wainwright@bbc.co.uk>
2019-03-16 00:02:26 +0000

    remove plist

--- Formula/bash-language-server.rb
+++ Formula/bash-language-server.rb
@@ -5,30 +5,6 @@ class BashLanguageServer < Formula
   sha256 "3ecc8b0d8529bb1f2db06eaa7f47ff3846065fe439ee35e458bbd106a8f09abd"
   depends_on "node"
 
-  def plist; <<~EOS
-  <?xml version="1.0" encoding="UTF-8"?>
-  <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-  <plist version="1.0">
-  <dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>ProgramArguments</key>
-      <array>
-          <string>/usr/local/bin/node</string>
-          <string>/usr/local/bin/bash-language-server</string>
-      </array>
-      <key>RunAtLoad</key>
-      <true/>
-      <key>StandardErrorPath</key>
-      <string>/tmp/bash-language-server.stderr</string>
-      <key>StandardOutPath</key>
-      <string>/tmp/bash-language-server.stdout</string>
-  </dict>
-  </plist>
-  EOS
-  end
-
-
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
