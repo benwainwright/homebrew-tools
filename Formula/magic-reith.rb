3f42f46321e5ccbb48993f9d125d1b3be8425e3f
Ben Wainwright <ben.wainwright@bbc.co.uk>
2019-04-18 19:02:01 +0100

    Update version

--- Formula/magic-reith.rb
+++ Formula/magic-reith.rb
@@ -1,7 +1,7 @@
 class MagicReith < Formula
   desc 'Simply the BBC corporate proxy'
-  url 'https://github.com/benwainwright/magic-reith/archive/0.0.9.tar.gz'
-  sha256 '36ec5cd86c37825619aa65eb2c8e9850d9161a04581ff777c90b2ae087281160'
+  url 'https://github.com/benwainwright/magic-reith/archive/0.0.10.tar.gz'
+  sha256 '395408a3dc9c3db2b5c200b8722a13a60898c861633b99e6e250186adffd1370'
 
   plist_options :startup => true
 
