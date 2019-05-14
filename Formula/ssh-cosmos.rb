234cdc2c6fd4550bbef22a007a0d220d03950bfe
Ben Wainwright <ben.wainwright@bbc.co.uk>
2019-05-14 10:13:30 +0100

    Update ssh-cosmos to 0.0.10

--- Formula/ssh-cosmos.rb
+++ Formula/ssh-cosmos.rb
@@ -1,8 +1,8 @@
 
 class SshCosmos < Formula
   desc 'SSH wrapper for cosmos instances'
-  url 'https://github.com/benwainwright/ssh-cosmos/archive/0.0.8.tar.gz'
-  sha256 'ba418be3872b9f9aad404baaae854efbcd4f47e9e5b493308bb21edf5dd2559f'
+  url 'https://github.com/benwainwright/ssh-cosmos/archive/0.0.10.tar.gz'
+  sha256 '4ee2f72cb65c85ea2fbb44f80db1900d4539c574fee6a9252166b426da5f3b68'
 
   def install
     libexec.install Dir["libexec/*"]
