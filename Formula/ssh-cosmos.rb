1433bcfd148e6be5d3f740f2cfd9066962eaf140
Ben Wainwright <ben.wainwright@bbc.co.uk>
2019-05-14 09:52:20 +0100

    Fixed typos

--- Formula/ssh-cosmos.rb
+++ Formula/ssh-cosmos.rb
@@ -4,10 +4,6 @@ class SshCosmos < Formula
   url 'https://github.com/benwainwright/ssh-cosmos/archive/0.0.0.tar.gz'
   sha256 '988f72c37c9c8755758e66621ca5329ce03c31e88e22ad73d48d097435f37415'
 
-  plist_options :startup => true
-
-  depends_on "coreutils"
-
   def install
     libexec.install Dir["libexec/*"]
     prefix.install "ssh-cosmos"
