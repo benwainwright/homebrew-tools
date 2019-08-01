b456f8bff93ef10ba74af9101b0a990f407039c8
Ben Wainwright <ben.wainwright@bbc.co.uk>
2019-08-01 23:58:08 +0100

    Added reithd

---
+++ Formula/reithd.rb
@@ -0,0 +1,5 @@
+class ReithD < Formula
+  desc 'Never have to think about Reith again'
+  url 'https://github.com/benwainwright/reithd/releases/download/0.0.1/reithd'
+  sha256 'd48594a157edd3f561b4c050e55d85db72020d9c5f6725e47cb9836bd59205ee'
+end
