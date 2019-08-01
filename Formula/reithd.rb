9934ac0230e1541012f0eb4992d572f130927b1e
Ben Wainwright <ben.wainwright@bbc.co.uk>
2019-08-02 00:05:55 +0100

    Add launchagent

--- Formula/reithd.rb
+++ Formula/reithd.rb
@@ -7,4 +7,28 @@ class Reithd < Formula
     bin.install "reithd"
   end
 
+  plist_options :startup => true
+
+  def plist; <<~EOS
+  <?xml version="1.0" encoding="UTF-8"?>
+  <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+  <plist version="1.0">
+  <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+          <string>/usr/local/bin/reithd</string>
+          <string>start</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>StandardErrorPath</key>
+      <string>/tmp/reithd.stderr</string>
+      <key>StandardOutPath</key>
+      <string>/tmp/reithd.stdout</string>
+  </dict>
+  </plist>
+  EOS
+  end
 end
