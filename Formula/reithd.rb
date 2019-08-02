823c1e23cff0fa169e7f58d01352552b6cf89512
Ben Wainwright <ben.wainwright@bbc.co.uk>
2019-08-02 08:54:31 +0100

    Add new version of reithd

--- Formula/reithd.rb
+++ Formula/reithd.rb
@@ -1,8 +1,7 @@
 class Reithd < Formula
   desc 'Never have to think about Reith again'
-  url 'https://github.com/benwainwright/reithd/releases/download/0.0.1/reithd'
-  sha256 'd48594a157edd3f561b4c050e55d85db72020d9c5f6725e47cb9836bd59205ee'
-
+  url 'https://github.com/benwainwright/reithd/releases/download/0.1.0/reithd'
+  sha256 'e4b80ab733903296c75e5811c53af09f0cb5b473e436e002105732f6dfc44ea1'
   def install
     bin.install "reithd"
   end
