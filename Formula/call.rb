9731a651b8bb52791ba956c35d6e4450d53326c4
Ben Wainwright <ben.wainwright@bbc.co.uk>
2019-09-30 17:03:22 +0100

    Switch to single quotes

--- Formula/call.rb
+++ Formula/call.rb
@@ -2,8 +2,8 @@ class Call < Formula
   desc 'Bootstrapping API calls'
   homepage 'https://github.com/benwainwright/call'
   url 'https://github.com/benwainwright/call'
-  sha256 ""
-  depends_on "python"
+  sha256 ''
+  depends_on 'python'
 
   def install
     virtualenv_install_with_resources
